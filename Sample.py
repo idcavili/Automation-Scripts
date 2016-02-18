6e37b9430f43ce6cc7e6359d5335a460eb42d535
idcavili <Ian.Cavilia@gmail.com>
2016-02-18 14:04:01 -0500

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,8 +1,16 @@
 def avgRoomAirTemp():
   RATemp = (RATempN + RATempS + RATempE + RATempW) / 4
+  calcRAHeat()
+  calcRACool()
 
 def calcRAHeat():
   RAHeatPercent = RAHeatPID.update(RATemp)
   DAHeatTemp = map(RAHeatPercent)
   DAHeatSP = max(DAHeatTemp, DACoolSP)
+  DAHeatPID.setPoint(DAHeatSP)
   calcDAHeat()
+  
+def calcDAHeat():
+  DAHeatPercent = DAHeatPID.update(DAHeatSense)
+  HWSetPoint = map(DAHeatPercent)
+  requests.put(heat_valve/valve/pv,DAHeatPercent)
