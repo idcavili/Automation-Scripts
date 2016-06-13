956b095f60b46e39e0fba92b45f6925d16e7601a
idcavili <Ian.Cavilia@gmail.com>
2016-06-13 18:08:31 -0400

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -2,6 +2,7 @@ def avgRoomAirTemp():
   RATemp = (RATempN + RATempS + RATempE + RATempW) / 4
   calcRAHeat()
   calcRACool()
+  updateVAV()
 
 def calcRAHeat():
   RAHeatPercent = RAHeatPID.update(RATemp)
@@ -14,3 +15,24 @@ def calcDAHeat():
   DAHeatPercent = DAHeatPID.update(DAHeatSense)
   HWSetPoint = map(DAHeatPercent)
   requests.put(heat_valve/valve/pv,DAHeatPercent)
+  
+def calcRACool():
+  RACoolPercent = RACoolPID.update(RATemp)
+  DACoolTemp = map(RACoolPercent)
+  DACoolSP = min(DACoolTemp, DADehumSP)
+  DACoolPID.setPoint(DACoolSP)
+  calcDACool()
+  
+def calcDACool():
+  DACoolPercent = DACoolPID.update(DACoolSense)
+  CWSetPoint = map(DACoolPercent)
+  if supplyTemp > DACoolSense:
+    ervRatio = 0
+    requests.put(cool_valve/valve/pv, DACoolPercent)
+  elif supplyTemp <= DACoolSense & supplyTemp > DACoolSP:
+    ervRatio = 100
+    requests.put(cool_valve/valve/pv, DACoolPercent)
+  elif supplyTemp <= DACoolSP:
+    ervRatio = DACoolPercent
+    requests.put(cool_valve/valve/pv, 0)
+
